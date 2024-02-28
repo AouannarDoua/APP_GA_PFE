1b123f8cd9b00dbb79ceaa87457cc22551fe76ba
fati <lemkademfatimazahraa@gmail.com>
2024-02-28 10:00:57 +0100

    teacher db created

--- app/src/main/java/com/example/app_ga_pfe/choix_du_profil.java
+++ app/src/main/java/com/example/app_ga_pfe/choix_du_profil.java
@@ -2,13 +2,46 @@ package com.example.app_ga_pfe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class choix_du_profil extends AppCompatActivity {
-
+    ImageView teacher ;
+    ImageView student ;
+    ImageView admin ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        teacher=findViewById(R.id.teacher);
+        student=findViewById(R.id.student);
+        admin=findViewById((R.id.admin));
+
+        teacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(choix_du_profil.this , MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        student.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(choix_du_profil.this,MainActivity.class);
+                startActivity(intent);
+            }
+        }));
+
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(choix_du_profil.this , MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
