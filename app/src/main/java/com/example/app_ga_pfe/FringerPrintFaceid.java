b8f7a0106d471b03c9ba572f5a89f7ea27654625
LENOVO <daouannar20@gmail.com>
2024-03-01 12:13:58 +0100

    nouvelle4

--- app/src/main/java/com/example/app_ga_pfe/FringerPrintFaceid.java
+++ app/src/main/java/com/example/app_ga_pfe/FringerPrintFaceid.java
@@ -10,6 +10,8 @@ import androidx.biometric.BiometricPrompt;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -39,6 +41,7 @@ public class FringerPrintFaceid extends AppCompatActivity {
         // Utiliser les valeurs récupérées comme nécessaire
         if (isFaceIdActivated) {
             Toast.makeText(this, "Face ID est activé", Toast.LENGTH_SHORT).show();
+            Faceid();
 
         } else {
             // Face ID n'est pas activé
@@ -101,4 +104,73 @@ public class FringerPrintFaceid extends AppCompatActivity {
                 .build();
 
         biometricPrompt.authenticate(promptInfo);
-    }}
+    }
+    private void Faceid() {
+            BiometricManager biometricManager = BiometricManager.from(this);
+            switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
+                case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                    Toast.makeText(getApplicationContext(), "Device doesn't support Face ID", Toast.LENGTH_SHORT).show();
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                    Toast.makeText(getApplicationContext(), "Face ID not available", Toast.LENGTH_SHORT).show();
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                    // Prompt the user to enroll Face ID using a custom dialog or explanation.
+                    showEnrollFaceIDDialog();
+                    break;
+            }
+            executor = ContextCompat.getMainExecutor(this);
+            biometricPrompt = new BiometricPrompt(FringerPrintFaceid.this, executor, new BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    Toast.makeText(getApplicationContext(), "Authentication error: " + errString, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    Toast.makeText(getApplicationContext(), "Login Success!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(FringerPrintFaceid.this, MainActivity2.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                    Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("Biometric login for app ABscentEasy")
+                    .setDescription("Use Face ID To Login")
+                    .setSubtitle("Log in using your biometric credential")
+                    .setNegativeButtonText("Use account password")
+                    .build();
+
+            biometricPrompt.authenticate(promptInfo);
+        }
+
+        private void showEnrollFaceIDDialog() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Enroll Face ID");
+            builder.setMessage("Please enroll Face ID for biometric authentication.");
+            builder.setPositiveButton("Enroll", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Open device settings where the user can enroll Face ID manually.
+                    Intent intent = new Intent(Settings.ACTION_SECURITY_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+            builder.show();
+        }
+
+
+    }
